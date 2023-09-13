2beeeab24e9b21310fad001dc04743f31c6c1596
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2023-09-13 21:28:08 +0800

    fix(discord): don't re-fetch metadata on every poll

--- src/cmd/discord.rs
+++ src/cmd/discord.rs
@@ -14,37 +14,26 @@ use crate::{
 };
 
 #[derive(Debug, Clone)]
-struct Song {
-    id: String,
-    name: String,
-    artist: String,
-    album: String,
-    duration: f64,
-    album_artwork: String,
-    artist_artwork: Option<String>,
-    share_url: String,
-    share_id: String,
-}
-
-#[derive(Debug, Clone)]
-struct SongWithProgress {
-    song: Option<Song>,
-    position: Option<f64>,
-}
-
-#[derive(Debug, Clone)]
 struct ActivityConnection {
     last_song_id: Option<String>,
     last_position: Option<f64>,
     is_idle: bool,
 }
 
-async fn get_now_playing() -> Result<SongWithProgress> {
+async fn update_presence(
+    client: &mut DiscordIpcClient,
+    http_client: &reqwest::Client,
+    activity: &mut ActivityConnection,
+) -> Result<()> {
     if !music::is_running().await? {
-        return Ok(SongWithProgress {
-            song: None,
-            position: None,
-        });
+        if !activity.is_idle {
+            println!("{} any songs", "Not playing".yellow());
+            activity.last_position = None;
+            activity.last_song_id = None;
+            activity.is_idle = true;
+        }
+
+        return Ok(());
     };
 
     let initial_state = music::tell("get {player position, player state}").await?;
@@ -59,10 +48,14 @@ async fn get_now_playing() -> Result<SongWithProgress> {
         .ok_or_else(|| anyhow!("Could not obtain player state"))?;
 
     if state != "playing" {
-        return Ok(SongWithProgress {
-            song: None,
-            position: None,
-        });
+        if !activity.is_idle {
+            println!("{} any songs", "Not playing".yellow());
+            activity.last_position = None;
+            activity.last_song_id = None;
+            activity.is_idle = true;
+        }
+
+        return Ok(());
     }
 
     let position = position.parse::<f64>()?;
@@ -71,108 +64,68 @@ async fn get_now_playing() -> Result<SongWithProgress> {
         .await?
         .ok_or_else(|| anyhow!("Could not obtain track information"))?;
 
-    let client = reqwest::Client::new();
-
-    let metadata = music::get_metadata(&client, &track).await?;
-
-    Ok(SongWithProgress {
-        song: Some({
-            Song {
-                id: track.id,
-                name: track.name,
-                artist: track.artist,
-                album: track.album,
-                duration: track.duration,
-                album_artwork: metadata.album_artwork,
-                artist_artwork: metadata.artist_artwork,
-                share_url: metadata.share_url,
-                share_id: metadata.id,
-            }
-        }),
-        position: Some(position),
-    })
-}
-
-async fn update_presence(
-    client: &mut DiscordIpcClient,
-    activity: &mut ActivityConnection,
-) -> Result<()> {
-    let now: SongWithProgress = get_now_playing().await?;
-
     let mut ongoing = false;
 
-    if let Some(song) = now.song {
-        if let Some(last_song_id) = &activity.last_song_id {
-            if *last_song_id == song.id {
-                if let Some(last_position) = activity.last_position {
-                    if let Some(now_position) = now.position {
-                        if last_position <= now_position {
-                            ongoing = true;
-                        }
-                    }
+    if let Some(last_song_id) = &activity.last_song_id {
+        if *last_song_id == track.id {
+            if let Some(last_position) = &activity.last_position {
+                if last_position <= &position {
+                    ongoing = true;
                 }
             }
         }
-
-        if !ongoing {
-            let position = now
-                .position
-                .ok_or_else(|| anyhow!("Could not obtain position data from result"))?;
-
-            activity.last_position = Some(position);
-            activity.last_song_id = Some(song.id);
-            activity.is_idle = false;
-
-            println!(
-                "{} {} - {}",
-                "Song updated".blue(),
-                &song.name,
-                &song.artist
-            );
-
-            let now_ts = chrono::offset::Local::now().timestamp();
-            let start_ts = (now_ts as f64) - position;
-            let end_ts = (now_ts as f64) + song.duration - position;
-
-            let activity_state = format!("{} · {}", &song.artist, &song.album);
-
-            let mut activity = Activity::new().details(&song.name).state(&activity_state);
-
-            let mut activity_assets = Assets::new();
-
-            activity_assets = activity_assets
-                .large_image(&song.album_artwork)
-                .large_text(&song.name);
-
-            if let Some(artist_artwork) = song.artist_artwork {
-                activity_assets = activity_assets
-                    .small_image(&artist_artwork)
-                    .small_text(&song.artist);
-            }
-
-            activity = activity.assets(activity_assets);
-            activity = activity.timestamps(
-                Timestamps::new()
-                    .start(start_ts.floor() as i64)
-                    .end(end_ts.ceil() as i64),
-            );
-
-            activity = activity.buttons(vec![
-                Button::new("Listen on Apple Music", &song.share_url),
-                Button::new(
-                    "View on SongLink",
-                    &format!("https://song.link/i/{}", song.share_id),
-                ),
-            ]);
-
-            client.set_activity(activity).await?;
-        }
-    } else if !activity.is_idle {
-        println!("{} any songs", "Not playing".yellow());
-        activity.last_position = None;
-        activity.last_song_id = None;
-        activity.is_idle = true;
     }
+
+    if !ongoing {
+        activity.last_position = Some(position);
+        activity.last_song_id = Some(track.id.clone());
+        activity.is_idle = false;
+
+        println!(
+            "{} {} - {}",
+            "Song updated".blue(),
+            &track.name,
+            &track.artist
+        );
+
+        println!("need metadata now");
+        let metadata = music::get_metadata(&http_client, &track).await?;
+
+        let now_ts = chrono::offset::Local::now().timestamp();
+        let start_ts = (now_ts as f64) - position;
+        let end_ts = (now_ts as f64) + track.duration - position;
+
+        let activity_state = format!("{} · {}", &track.artist, &track.album);
+
+        let mut activity = Activity::new().details(&track.name).state(&activity_state);
+
+        let mut activity_assets = Assets::new()
+            .large_image(&metadata.album_artwork)
+            .large_text(&track.name);
+
+        if let Some(artist_artwork) = metadata.artist_artwork {
+            activity_assets = activity_assets
+                .small_image(&artist_artwork)
+                .small_text(&track.artist);
+        }
+
+        activity = activity.assets(activity_assets);
+        activity = activity.timestamps(
+            Timestamps::new()
+                .start(start_ts.floor() as i64)
+                .end(end_ts.ceil() as i64),
+        );
+
+        activity = activity.buttons(vec![
+            Button::new("Listen on Apple Music", &metadata.share_url),
+            Button::new(
+                "View on SongLink",
+                &format!("https://song.link/i/{}", track.id),
+            ),
+        ]);
+
+        client.set_activity(activity).await?;
+    };
 
     Ok(())
 }
@@ -189,12 +142,14 @@ pub async fn discord() -> Result<()> {
         is_idle: false,
     };
 
+    let http_client = reqwest::Client::new();
+
     let mut intvl = tokio::time::interval(Duration::from_secs(5));
 
     loop {
         tokio::select! {
             _ = intvl.tick() => {
-                if let Err(err) = update_presence(&mut client, &mut activity).await {
+                if let Err(err) = update_presence(&mut client,& http_client, &mut activity).await {
                     eprintln!("{} {}", "Error".red(), err);
                 }
             }
