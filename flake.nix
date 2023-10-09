62fd0a762f8641889225f47a52577ae34f3bd1ba
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2023-10-09 20:08:31 +0800

    add SystemConfiguration framework to Nix package

--- flake.nix
+++ flake.nix
@@ -57,7 +57,7 @@
     overlays.default = _: prev: {
       am = prev.callPackage ./default.nix {
         inherit self version;
-        inherit (prev.darwin.apple_sdk_11_0.frameworks) CoreFoundation Security;
+        inherit (prev.darwin.apple_sdk_11_0.frameworks) CoreFoundation Security SystemConfiguration;
         inherit (prev.darwin) IOKit;
       };
     };
